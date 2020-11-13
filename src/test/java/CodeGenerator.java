c6acb9fe0b661bb2509f60df2b5e059e5cc434a6
QTMY <yq1271309617@gmail.com>
2020-11-13 09:39:06 +0800

    修改实体类生成不会生成id属性与@id注解

--- src/test/java/CodeGenerator.java
+++ src/test/java/CodeGenerator.java
@@ -18,10 +18,10 @@ import static com.company.project.core.ProjectConstant.*;
  */
 public class CodeGenerator {
     //JDBC配置，请修改为你项目的实际配置
-    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/smbms?characterEncoding=utf8&serverTimezone=Asia/Shanghai";
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/game?useSSL=false";
     private static final String JDBC_USERNAME = "root";
     private static final String JDBC_PASSWORD = "1271309617";
-    private static final String JDBC_DIVER_CLASS_NAME = "com.mysql.cj.jdbc.Driver";
+    private static final String JDBC_DIVER_CLASS_NAME = "com.mysql.jdbc.Driver";
 
     private static final String PROJECT_PATH = System.getProperty("user.dir");//项目在硬盘上的基础路径
     private static final String TEMPLATE_FILE_PATH = PROJECT_PATH + "/src/test/resources/generator/template";//模板位置
@@ -39,7 +39,7 @@ public class CodeGenerator {
     public static void main(String[] args) {
         // 多表同时生成
         // genCode("表名1","表名2");
-        genCode("smbms_user","smbms_role");
+        genCode("users");
         //genCodeByCustomModelName("输入表名","输入自定义Model名称");
     }
 
